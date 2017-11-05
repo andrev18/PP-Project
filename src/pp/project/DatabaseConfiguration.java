7b34840de7ad40b6260d3c20447cbcb76ec9955c
Andrei Vlad <vladandrei92@gmail.com>
2017-11-05 15:44:56 +0200

    init

---
+++ src/pp/project/DatabaseConfiguration.java
@@ -0,0 +1,5 @@
+package pp.project;
+
+public class DatabaseConfiguration {
+
+}
---
+++ src/pp/project/HashList.java
@@ -0,0 +1,10 @@
+package pp.project;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class HashList{
+
+
+
+}
